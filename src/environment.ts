62a1bcd71c83e6faf8a057353cc1a54b0c70ba90
Guilherme <guilhermevaccaro6@gmail.com>
2024-05-21 22:02:29 -0300

    new env backup

--- src/environment.ts
+++ src/environment.ts
@@ -1,25 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDqa3rxJhvLdOclZBdCqgre05-M6GRrt6A',
-    authDomain: 'meuteste-59f6d.firebaseapp.com',
-    projectId: 'meuteste-59f6d',
-    storageBucket: 'meuteste-59f6d.appspot.com',
-    messagingSenderId: 'S296649156651',
-    appId: '1:296649156651:web:a540ae438e83c7ee11e26e',
-    measurementId: 'G-VL01N6SPHJ',
+    apiKey: 'AIzaSyDjXyC9wk9zBC0MDr-DaTdH71qLL9SwF0o',
+    authDomain: 'controle-financeiro-publico.firebaseapp.com',
+    projectId: 'controle-financeiro-publico',
+    storageBucket: 'controle-financeiro-publico.appspot.com',
+    messagingSenderId: '20961223181',
+    appId: '1:20961223181:web:3a13f6b3cf42b8bb33ab44',
   },
 };
-// ENV PRA TESTE
-// export const environment = {
-//   production: false,
-//   firebaseConfig: {
-//     apiKey: "AIzaSyCYIn9Qe1pyGKstu-WiTuhmIHgX6BXEPJ0",
-//   authDomain: "barbeariafamilystyle.firebaseapp.com",
-//   projectId: "barbeariafamilystyle",
-//   storageBucket: "barbeariafamilystyle.appspot.com",
-//   messagingSenderId: "835620718533",
-//   appId: "1:835620718533:web:eb69fcb288b140c3686dac",
-//   measurementId: "G-6XH8WQF6QP"
-//   },
-// };
